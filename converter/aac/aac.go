af9ebedabf092954c3f470997a2b1a04403cd945
Edney Osf <edney.osf@gmail.com>
2025-03-14 12:40:12 -0300

    Improved 5.1 to stereo downmixing in AAC

--- converter/aac/aac.go
+++ converter/aac/aac.go
@@ -67,7 +67,8 @@ func channelsHandler(channelsIn string) (string, []string) {
 		af = append(af, "-af")
 		//af = append(af, "pan=stereo| FL=0.5*FL + 0.5*FC + 0.5*SL + 0.5*LFE | FR=0.5*FR + 0.5*FC + 0.5*SR + 0.5*LFE")
 		//af = append(af, "pan=stereo|FL=0.8*FL+0.5*FC+0.3*SL+0.3*LFE|FR=0.8*FR+0.5*FC+0.3*SR+0.3*LFE")
-		af = append(af, "lowpass=c=LFE:f=120,volume=1.6,pan=stereo|FL=0.5*FC+0.707*FL+0.707*BL+0.5*LFE|FR=0.5*FC+0.707*FR+0.707*BR+0.5*LFE")
+		//af = append(af, "lowpass=c=LFE:f=120,volume=1.6,pan=stereo|FL=0.5*FC+0.707*FL+0.707*BL+0.5*LFE|FR=0.5*FC+0.707*FR+0.707*BR+0.5*LFE")
+		af = append(af, "lowpass=c=LFE:f=120,volume=1.6,pan=stereo|FL=0.8*FL+0.5*FC+0.6*BL+0.4*LFE|FR=0.8*FR+0.5*FC+0.6*BR+0.4*LFE")
 	case "6":
 		channel = "6"
     default:
